3b416a17c8399de5e0bf52aa6678bceecc166c29
tomaalex955 <talex4259@gmail.com>
2020-04-29 17:02:49 +0300

    Init project

---
+++ TransportLogistics/TransportLogistics/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TransportLogistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
