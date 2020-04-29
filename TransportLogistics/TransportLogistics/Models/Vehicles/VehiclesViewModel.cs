8bf81bd3e74d049c1fe04f7e4c4ac71229b93a9a
Popescu Alexandru <56479261+alexpopescu021@users.noreply.github.com>
2020-04-29 18:18:17 +0300

    Start-Up

---
+++ TransportLogistics/TransportLogistics/Models/Vehicles/VehiclesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransportLogistics.Models.Vehicles
+{
+    public class VehiclesViewModel
+    {
+        public string VehicleName { get; set; }
+        public int NumberOfTrailers { get; set; }
+        // aici sau la driver: currentlocation, status of departure
+    }
+}
