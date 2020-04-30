4a3ce3a7762e6791f01861414217db56feb3f7af
Toma Mihnea <toma.mihnea9923@gmail.com>
2020-04-30 13:04:46 +0300

    Mihnea branch

--- TransportLogistics/TransportLogistics/Controllers/HomeController.cs
+++ TransportLogistics/TransportLogistics/Controllers/HomeController.cs
@@ -27,6 +27,10 @@ namespace TransportLogistics.Controllers
         {
             return View();
         }
+        public IActionResult Test()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
