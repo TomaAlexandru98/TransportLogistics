f1c8964bafa051ba5c1bfc94291c780034c427c5
Popescu Alexandru <56479261+alexpopescu021@users.noreply.github.com>
2020-04-30 13:03:56 +0300

    DummyTable

--- TransportLogistics/TransportLogistics/Models/Vehicles/VehiclesViewModel.cs
+++ TransportLogistics/TransportLogistics/Models/Vehicles/VehiclesViewModel.cs
@@ -8,7 +8,10 @@ namespace TransportLogistics.Models.Vehicles
     public class VehiclesViewModel
     {
         public string VehicleName { get; set; }
+        public string VehicleType { get; set; }
         public int NumberOfTrailers { get; set; }
+        public int MaxmimumNoTrailers { get; set; }
+
         // aici sau la driver: currentlocation, status of departure
     }
 }
