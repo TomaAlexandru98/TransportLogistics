b4ca3b3828c8c027b801486438955b013275216a
tomaalex955 <talex4259@gmail.com>
2020-05-04 11:36:04 +0300

    Persistence context

---
+++ TransportLogistics/TransportLogistics.Data/Abstractions/IPersistenceContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace TransportLogistics.Data.Abstractions
+{
+    public interface IPersistenceContext : IDisposable
+    {
+        TransactionScope BeginTransaction();
+        void SaveChanges();
+    }
+}
---
+++ TransportLogistics/TransportLogistics.DataAccess/Repositories/EFPersistenceContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+using TransportLogistics.Data.Abstractions;
+
+namespace TransportLogistics.DataAccess.Repositories
+{
+    public class EFPersistenceContext : IPersistenceContext
+    {
+        private readonly TransportLogisticsDbContext dbContext;
+        private TransactionScope currentTransactionScope = null;
+        public EFPersistenceContext(TransportLogisticsDbContext context)
+        {
+            this.dbContext = context;
+        }
+
+        public TransactionScope BeginTransaction()
+        {
+            if (currentTransactionScope != null)
+            {
+                throw new TransactionException("A transaction is already in progress for this context");
+            }
+            currentTransactionScope = new TransactionScope(TransactionScopeOption.Required,
+                new TransactionOptions { IsolationLevel = IsolationLevel.ReadCommitted });
+
+            return currentTransactionScope;
+        }
+
+        public void Dispose()
+        {
+
+            dbContext.Dispose();
+        }
+
+        public void SaveChanges()
+        {
+            dbContext.SaveChanges();
+            if (currentTransactionScope != null)
+            {
+                currentTransactionScope.Complete();
+            }
+
+            currentTransactionScope = null;
+        }
+    }
+}
