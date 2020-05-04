a3dd641ffdb0730bbc77513213fe501930d68c65
Papana Dan-Cristian <38207553+DanPapana@users.noreply.github.com>
2020-05-04 22:19:21 +0300

    CustomerRepository added

---
+++ TransportLogistics/TransportLogistics.DataAccess.Abstractions/ICustomerRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TransportLogistics.Model;
+
+namespace TransportLogistics.DataAccess.Abstractions
+{
+    public interface ICustomerRepository : IBaseRepository<Customer>
+    {
+        IEnumerable<Customer> FindByName(string nameToFind);
+        Customer FindByEmail(string emailToFind);
+        Customer FindByPhoneNo(string phoneNo);
+    }
+}
