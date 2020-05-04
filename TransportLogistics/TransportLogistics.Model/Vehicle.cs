b3105f87d155f4c01ed92ecde9f92bc2b26a1ea7
Toma Mihnea <toma.mihnea9923@gmail.com>
2020-05-04 17:02:45 +0300

    repos

--- TransportLogistics/TransportLogistics.Model/Vehicle.cs
+++ TransportLogistics/TransportLogistics.Model/Vehicle.cs
@@ -12,5 +12,25 @@ namespace TransportLogistics.Model
         public ICollection<Trailer> CurrentTrailers { get; protected set; }
         public int MaximCarryWeightKg { get; protected set; }
         public string VIN { get; protected set; }
+        public static Vehicle Create(string Name, string Type, int MaximCarryWeight, string VIN)
+        {
+            if (Name == null || Type == null || VIN == null)
+            {
+                throw new ArgumentNullException();
+            }
+            if (MaximCarryWeight < 0)
+            {
+                throw new Exception("Maxim carry weight can not be less than 0");
+            }
+            return new Vehicle
+            {
+                Name = Name,
+                Type = Type,
+                MaximCarryWeightKg = MaximCarryWeight,
+                VIN = VIN,
+                Id = Guid.NewGuid(),
+               // CurrentTrailers = new List<TrailerData>()
+            };
+        }
     }
 }
