91e94d7cd55fcaad41fe1252879d1930809f2089
Popescu Alexandru <56479261+alexpopescu021@users.noreply.github.com>
2020-05-04 16:43:57 +0300

    .

--- TransportLogistics/TransportLogistics.Model/Trailer.cs
+++ TransportLogistics/TransportLogistics.Model/Trailer.cs
@@ -14,8 +14,43 @@ namespace TransportLogistics.Model
         public decimal Height { get; protected set; }
         public decimal Width { get; protected set; }
         public decimal Length { get; protected set; }
+        protected Trailer()
+        {
+        }
+        public static Trailer Create(string model, int maximumWeightKg, int capacity, int numberAxles, decimal height, decimal width, decimal length)
+        {
+            if (string.IsNullOrEmpty(model))
+                throw new ArgumentException("");
+            if (maximumWeightKg <= 0)
+                throw new ArgumentException("");
+            if (capacity <= 0)
+                throw new ArgumentException("");
+            if (numberAxles <= 0)
+                throw new ArgumentException("");
+            if (height <= 0)
+                throw new ArgumentException("");
+            if (width <= 0)
+                throw new ArgumentException("");
+            if (length <= 0)
+                throw new ArgumentException("");
+            var trailer = new Trailer
+            {
+                Id = Guid.NewGuid(),
+                Model = model,
+                MaximWeightKg = maximumWeightKg,
+                Capacity = capacity,
+                NumberAxles = numberAxles,
+                Height = height,
+                Width = width,
+                Length = length
+            };
+            return trailer;
+        }
+        public void Modify(int aux)
+        {
 
-        public virtual VehicleData Vehicle { get; protected set; }
+        }
+        public virtual Vehicle Vehicle { get; protected set; }
 
 
 
