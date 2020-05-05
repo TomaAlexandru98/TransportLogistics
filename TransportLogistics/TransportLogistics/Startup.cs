d461588f40ef1b244d8c39de17b4b18dd3e76059
Popescu Alexandru <56479261+alexpopescu021@users.noreply.github.com>
2020-05-05 16:45:23 +0300

    AddRuntimeCompilation

--- TransportLogistics/TransportLogistics/Startup.cs
+++ TransportLogistics/TransportLogistics/Startup.cs
@@ -39,7 +39,7 @@ namespace TransportLogistics
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
