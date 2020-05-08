a66eb108109c75093a00eae887eac1fe9c6f3bd4
mihnea9923 <54399158+mihnea9923@users.noreply.github.com>
2020-05-08 15:40:28 +0300

    com

---
+++ TransportLogistics/TransportLogistics.DataAccess.Abstractions/ICustomerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TransportLogistics.Model;
+
+namespace TransportLogistics.DataAccess.Abstractions
+{
+    public interface ICustomerRepository : IBaseRepository<Customer>
+    {
+        IEnumerable<Customer> FindByLastName(string nameToFind);
+        Customer FindByEmail(string emailToFind);
+        Customer FindByPhoneNo(string phoneNo);
+        Customer GetCustomerByGuid(Guid customerId);
+        bool RemoveCustomerWithLocations(Guid customerId);
+        void AddLocationToCustomer(Guid customerId, LocationAddress address);
+        Customer UpdateCustomer(Guid customerId, string name, string phoneNo, string email);
+    }
+}
---
+++ TransportLogistics/TransportLogistics.DataAccess.Abstractions/IVehicleRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TransportLogistics.Model;
+
+namespace TransportLogistics.DataAccess.Abstractions
+{
+    public interface IVehicleRepository : IBaseRepository<Vehicle>
+    {
+    }
+}
