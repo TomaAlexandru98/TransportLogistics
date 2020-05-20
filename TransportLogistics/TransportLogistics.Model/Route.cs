a51470b88e4cf2ffc69564c126bc5c71e5d94820
mihnea9923 <54399158+mihnea9923@users.noreply.github.com>
2020-05-20 19:07:20 +0300

    remove redundant migration

--- TransportLogistics/TransportLogistics.Model/Route.cs
+++ TransportLogistics/TransportLogistics.Model/Route.cs
@@ -7,6 +7,7 @@ namespace TransportLogistics.Model
     public class Route:DataEntity
     {
         public ICollection<RouteEntry> RouteEntries { get; private set; }
+       
         public static Route Create()
         {
             var route = new Route()
