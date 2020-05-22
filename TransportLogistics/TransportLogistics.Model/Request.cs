eae3441a54d24b8829f2098b935573390aa9d431
mihnea9923 <54399158+mihnea9923@users.noreply.github.com>
2020-05-22 23:21:59 +0300

    request

---
+++ TransportLogistics/TransportLogistics.Model/Request.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportLogistics.Model
+{
+    public class Request:DataEntity
+    {
+        public Driver Driver { get; private set; }
+        public Trailer Trailer { get; private set; }
+        public Supervisor Supervisor { get; private set; }
+    }
+}
