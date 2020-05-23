fc5c521aeddbe3e8887dbab4524729737ddd0cdf
tomaalex955 <talex4259@gmail.com>
2020-05-23 14:25:52 +0300

    Request done

--- TransportLogistics/TransportLogistics.DataAccess/Migrations/20200523112513_UpdateRequest.Designer.cs
+++ TransportLogistics/TransportLogistics.DataAccess/Migrations/20200523112513_UpdateRequest.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TransportLogistics.DataAccess;
 
 namespace TransportLogistics.DataAccess.Migrations
 {
     [DbContext(typeof(TransportLogisticsDbContext))]
-    partial class TransportLogisticsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200523112513_UpdateRequest")]
+    partial class UpdateRequest
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
